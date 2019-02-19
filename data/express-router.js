10e3e3ab75f5ea6bc8c3cd0cc0f914ac0b1c5eec
josharmantrout91 <josh.armantrout@gmail.com>
2019-02-19 18:27:28 -0500

    All GET requests, POST requests, and DELETE requests functional

--- data/express-router.js
+++ data/express-router.js
@@ -4,4 +4,61 @@ const db = require("./db.js");
 
 const router = express.Router();
 
+// POST to /api/posts - Creates a post using the information sent inside the request body.
+router.post("/", async (req, res) => {
+  try {
+    const post = await db.insert(req.body);
+
+    if (req.body.title && req.body.contents) {
+      res.status(201).json(post);
+    } else {
+      res.status(400).json({
+        errorMessage: "Please provide title and contents for the post."
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: "There was an error while saving the post to the database"
+    });
+  }
+});
+
+// GET to /api/posts - Returns an array of all the post objects contained in the database.
+router.get("/", async (req, res) => {
+  try {
+    const posts = await db.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    // log error to database
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "The posts information could not be retrieved." });
+  }
+});
+
+// GET to /api/posts/:id - Returns the post object with the specified id.
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await db.findById(req.params.id);
+
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res
+        .status(404)
+        .json({ error: "The posts information could not be retrieved." });
+    }
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "The post information could not be retrieved." });
+  }
+});
+
+// DELETE to /api/posts/:id	- Removes the post with the specified id and returns the deleted post object. You may need to make additional calls to the database in order to satisfy this requirement.
+
+// PUT to /api/posts/:id - Updates the post with the specified id using data from the request body. Returns the modified document, NOT the original.
+
 module.exports = router;
